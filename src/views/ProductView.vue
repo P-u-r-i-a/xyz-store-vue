54e9d51edd67539e229b18c6da857764e16e38b0
Puria Golgoonnejad <puriagit@gmail.com>
2022-03-26 18:01:09 +1100

    Implement Related products

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -5,6 +5,22 @@
     <p class="product-price">
       {{ `${product.price.amount} ${product.price.base}` }}
     </p>
+    <hr />
+    <h2>Related Products</h2>
+    <ul>
+      <li v-for="relatedProduct in relatedProducts" :key="relatedProduct.id">
+        <div>
+          <RouterLink
+            :to="{ name: 'product', params: { id: relatedProduct.id } }"
+          >
+            <p class="product-title">{{ relatedProduct.name }}</p>
+          </RouterLink>
+          <p class="product-price">
+            {{ `${relatedProduct.price.amount} ${relatedProduct.price.base}` }}
+          </p>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -13,6 +29,7 @@ export default {
   data() {
     return {
       product: null,
+      relatedProducts: [],
     };
   },
   created() {
@@ -22,6 +39,23 @@ export default {
   },
   mounted() {
     this.product = this.$store.getters["getProductById"](this.id);
+    this.handleRelatedProducts();
+  },
+  methods: {
+    handleRelatedProducts() {
+      this.product.relatedProducts.forEach((relatedProductId) => {
+        this.relatedProducts.push(
+          this.$store.getters["getProductById"](relatedProductId)
+        );
+      });
+    },
+  },
+  watch: {
+    id() {
+      this.product = this.$store.getters["getProductById"](this.id);
+      this.relatedProducts = [];
+      this.handleRelatedProducts();
+    },
   },
 };
 </script>
